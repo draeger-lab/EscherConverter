cdfe2b7f61081107bf278c04d6fd92c1055ac0ce
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-06-07 17:50:10 +0530

    Update Unit dependency. Sample test run.

---
+++ src/test/java/edu/ucsd/sbrg/escher/SampleTest.java
@@ -0,0 +1,16 @@
+package edu.ucsd.sbrg.escher;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Devesh Khandelwal on 07/06/16.
+ */
+public class SampleTest {
+
+  @Test
+  public void evaluatesExpression() {
+    assertEquals(6, 2*3);
+  }
+}
