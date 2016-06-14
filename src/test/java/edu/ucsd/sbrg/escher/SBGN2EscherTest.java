11471f5ee5695d402deecb418218c6a00fbedfb3
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-06-14 10:26:22 +0530

    Oops. Forgot to add files to previous commit.(Tests)

---
+++ src/test/java/edu/ucsd/sbrg/escher/SBGN2EscherTest.java
@@ -0,0 +1,41 @@
+package edu.ucsd.sbrg.escher;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by deveshkhandelwal on 13/06/16.
+ */
+public class SBGN2EscherTest {
+
+  @BeforeClass
+  public static void createTheWorld() {
+
+  }
+
+  @Test
+  public void canGetCanvasInfo() {
+
+  }
+
+  @Test
+  public void canGetMetaInfo() {
+
+  }
+
+  @Test
+  public void canCreateNodes() {
+
+  }
+
+  @Test
+  public void canCreateReactions() {
+
+  }
+
+  @Test
+  public void canCreateTextLabels() {
+
+  }
+
+}
