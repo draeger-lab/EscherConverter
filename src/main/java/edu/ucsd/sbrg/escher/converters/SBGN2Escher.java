b6b31c6413a5bad1b05da0a6ff68fc8cfe33a2c6
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-06-16 11:37:06 +0530

    SBGN: Process glyphs.

--- src/main/java/edu/ucsd/sbrg/escher/converters/SBGN2Escher.java
+++ src/main/java/edu/ucsd/sbrg/escher/converters/SBGN2Escher.java
@@ -4,14 +4,42 @@ import edu.ucsd.sbrg.escher.model.EscherMap;
 import org.sbgn.bindings.Map;
 import org.sbgn.bindings.Sbgn;
 
+import java.util.logging.Logger;
+
 /**
  * Created by deveshkhandelwal on 13/06/16.
  */
 public class SBGN2Escher extends Standard2Escher<Sbgn> {
 
+  /**
+   * A {@link java.util.logging.Logger} for this class.
+   */
+  private static final Logger logger = Logger.getLogger(Standard2Escher.class.getName());
+
   private String determineComponent(String classs) {
     // TODO: Determine class according to the SBGN PD Level 1 spec draft.
-    throw new UnsupportedOperationException("Gotta wait, buddy!");
+    switch (classs) {
+
+    case "simple chemical":
+    case "perturbing agent":
+    case "unspecified entity":
+      return "node";
+
+    case "tag":
+    case "annotation":
+      return "text_label";
+
+    case "process":
+    case "omitted process":
+    case "uncertain process":
+    case "association":
+    case "dissociation":
+      return "reaction";
+
+    default:
+      return classs;
+
+    }
   }
 
   @Override
@@ -26,30 +54,27 @@ public class SBGN2Escher extends Standard2Escher<Sbgn> {
 
       case "node":
         // TODO: Call createNode and add to EscherMap properly.
+        escherMap.addNode(createNode(g));
         break;
 
       case "reaction":
         // TODO: Call createReaction and add to EscherMap properly.
-        break;
-
-      case "segment":
-        // TODO: Call createSegment and add to EscherMap properly.
+        escherMap.addNode(createNode(g));
+        escherMap.addReaction(createReaction(g));
         break;
 
       case "text_label":
         // TODO: Call createTextLabel and add to EscherMap properly.
-        break;
-
-      case "metabolite":
-        // TODO: Determine which metabolites to be added to which reaction.
+        escherMap.addTextLabel(createTextLabel(g));
         break;
 
       default:
         // TODO: Log a message saying unsupported class.
+        logger.warning(String.format("Glyph %s has unsupported class \"%s\"", g.getId(), g.getClazz()));
         break;
 
       }
     });
-    throw new UnsupportedOperationException("Not completed yet!");
+    throw new UnsupportedOperationException("Work in progress!");
   }
 }
