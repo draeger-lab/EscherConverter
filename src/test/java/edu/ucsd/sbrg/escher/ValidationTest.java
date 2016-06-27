5f146a2d9cd35fdc469abf5e2bf026ab1368d5fc
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-06-27 23:33:58 +0530

    Stub validation tests. Put schema files in resources.

---
+++ src/test/java/edu/ucsd/sbrg/escher/ValidationTest.java
@@ -0,0 +1,76 @@
+package edu.ucsd.sbrg.escher;
+
+import edu.ucsd.sbrg.escher.utilities.Validation;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * Created by Devesh Khandelwal on 27-06-2016.
+ */
+public class ValidationTest {
+
+  public Validation validation;
+  public File schemaFile;
+
+  @BeforeClass
+  public void createTheWorld() {
+
+  }
+
+
+  @Test
+  public void failsOnInvalidJsonSchemaFile() {
+
+  }
+
+
+  @Test
+  public void canValidateJsonSchemaFile() {
+
+  }
+
+
+  @Test
+  public void failsOnInvalidJsonEscherFile() {
+
+  }
+
+
+  @Test
+  public void validatesEscherJson() {
+
+  }
+
+
+  @Test
+  public void validatesSbgnDocument() {
+
+  }
+
+
+  @Test
+  public void failsOnInvalidSbgnDocument() {
+
+  }
+
+
+  @Test
+  public void failsWhenLanguageNotPresentOnSbgn() {
+
+  }
+
+
+  @Test
+  public void validatesSbmlDocument() {
+
+  }
+
+
+  @Test
+  public void failsOnInvalidSbmlDocument() {
+
+  }
+
+}
