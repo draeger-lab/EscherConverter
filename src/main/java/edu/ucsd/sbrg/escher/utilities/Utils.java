da8d46813dc61e7a037a83277a937f340f91ea1f
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-06-29 18:03:51 +0530

    Extract ObjectMapper creation and put in Utils.

--- src/main/java/edu/ucsd/sbrg/escher/utilities/Utils.java
+++ src/main/java/edu/ucsd/sbrg/escher/utilities/Utils.java
@@ -1,6 +1,12 @@
 package edu.ucsd.sbrg.escher.utilities;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.fge.jackson.JsonLoader;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 import com.github.fge.jsonschema.main.JsonSchema;
@@ -41,5 +47,25 @@ public class Utils {
     JsonNode jsonNode = JsonLoader.fromResource(bundle.getString("default_escher_schema_file"));
 
     return jsonNode;
+  }
+
+
+  public static ObjectMapper getObjectMapper() {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    objectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+    objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+    objectMapper.disable(MapperFeature.AUTO_DETECT_SETTERS);
+    objectMapper.disable(MapperFeature.AUTO_DETECT_CREATORS);
+    objectMapper.disable(MapperFeature.AUTO_DETECT_FIELDS);
+    objectMapper.disable(MapperFeature.AUTO_DETECT_GETTERS);
+    objectMapper.disable(MapperFeature.AUTO_DETECT_IS_GETTERS);
+    objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+    objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.NONE);
+    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+    return objectMapper;
   }
 }
