4ea365cd742c0133f222cae9fd90d624220368fe
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-07-21 23:46:48 +0530

    Stub end-points a bit more. Tests ignored.

--- api/api.py
+++ api/api.py
@@ -1,11 +1,15 @@
+import json
+
 from flask import Blueprint, jsonify
 from flask import request
+
+from models import ConvertRequest, OutputFormat
 
 api = Blueprint('api', __name__)
 
 
 @api.route('/convert/<req_id>', methods=['GET'])
-def home(req_id):
+def get_conversion_status(req_id):
     """
     Returns the meta info about a conversion request.
     :param req_id: The request id issued before.
@@ -19,9 +23,27 @@ def home(req_id):
 
 
 @api.route('/convert', methods=['POST'])
-def hello():
-    body = request.json
-    return jsonify(body)
+def conversion_request():
+    if request.content_type != 'application/json':
+        return jsonify({
+            'status': 'errored',
+            'message': 'Content-Type must be application/json'
+        }), 415
+    d = None
+    cr_object = None
+    try:
+        cr_object = ConvertRequest(request.json)
+    except:
+        return jsonify({
+            'status': 'errored',
+            'message': 'Invalid request JSON!!!'
+        }), 400
+    # TODO: Add conversion request to database.
+    # TODO: return job id.
+    return jsonify({
+        'status': 'errored',
+        'message': 'not yet implemented'
+    }), 501
 
 
 @api.route('/convert', methods=['GET'])
