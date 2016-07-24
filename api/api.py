bba3a05417527e5c1ec7c709a913b4d97300120c
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-07-24 23:10:57 +0530

    File upload done. Multiple files supported.

--- api/api.py
+++ api/api.py
@@ -1,12 +1,15 @@
+import os
 import uuid
 from datetime import datetime
 
 import time
 from flask import Blueprint, jsonify
 from flask import request
+from flask import app
 
 from config import config
 from database import Database
+from escherconverter import EscherConverter
 from models import ConvertRequest, OutputFormat, ConversionStatus
 
 api = Blueprint('api', __name__)
@@ -44,7 +47,7 @@ def get_conversion_status(req_id):
         'submission_date': datetime.fromtimestamp(cr.submission_date),
     }
     if cr.status == ConversionStatus.completed or cr.status == ConversionStatus.failed or \
-            cr.status == ConversionStatus.errored:
+                    cr.status == ConversionStatus.errored:
         resp['completion_date'] = datetime.fromtimestamp(cr.completion_date)
     return jsonify(resp), 200
 
@@ -76,6 +79,7 @@ def conversion_request():
             'id': cr_object.id,
             'status': cr_object.status.value,
             'submission_time': datetime.fromtimestamp(cr_object.submission_date),
+            'number_of_files': cr_object.file_count,
             'message': 'job submitted successfully, add files to start the conversion'
         })
         cr_object.status = ConversionStatus.waiting
@@ -86,6 +90,41 @@ def conversion_request():
             'status': 'errored',
             'message': r'can"t submit new job'
         }), 500
+
+
+@api.route('/convert/<req_id>/input/<file_number>', methods=['PUT', 'POST'])
+def add_file(req_id, file_number):
+    cr = db.retrieve(req_id)
+    if cr is None:
+        return jsonify({
+            'status': 'errored',
+            'message': 'no job found matching request id: ' + req_id
+        }), 404
+    if request.content_type not in ['application/json', 'application/xml',
+                                    'text/xml', 'text/plain']:
+        return jsonify({
+            'status': 'errored',
+            'message': 'Content-Type must be either of :' + "'application/xml', "
+                                                            "'application/json', "
+                                                            "'text/xml', 'text/plain'"
+        }), 415
+    if int(file_number) not in range(cr.file_count):
+        return jsonify({
+            'status': 'errored',
+            'message': 'file number must be in (0, ' + str(cr.file_count - 1) + ').'
+        }), 404
+    os.makedirs(config['FILE_STORE'] + req_id + '/input/', exist_ok=True)
+    file = open(config['FILE_STORE'] + req_id + '/input/' + file_number, 'wb')
+    file.write(request.stream.read())
+    cr.files_uploaded += 1
+    db.update()
+    if cr.files_uploaded != cr.file_count:
+        return jsonify({
+            'status': 'waiting',
+            'message': 'file successfully added, add all files to start the conversion.'
+        }), 200
+    # TODO: Check if all files have been uploaded, set status to running, then start the conversion.
+    # converter = EscherConverter()
 
 
 @api.route('/convert', methods=['GET'])
