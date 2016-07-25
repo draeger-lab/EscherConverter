c89f720a87fc6ec33a768050f4c863b1a36edd8a
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-07-25 15:40:41 +0530

    Conversion completing successfully.
    Although, the output path is output->input.

---
+++ api/utils.py
@@ -0,0 +1,9 @@
+
+def extension_from_content_type():
+    return {
+        'application/xml': '.xml',
+        'text/xml': '.xml',
+        'application/atom+xml': '.xml',
+        'application/json': '.json'
+        'text/json'
+    }
