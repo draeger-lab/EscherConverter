9bcab3516c2aaf6ba65463ff2e93e83c9e23ca8a
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-08-02 11:39:33 +0530

    Bootstrap front-end of the web-service.

---
+++ frontend/deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+# EscherConverter
+
+# git branch -f gh-pages
+# git checkout gh-pages
+# git reset --hard origin/master
+# npm run build
+# cp -r public/* .
+# git add -A .
+# git commit -a -m 'site update'
+# git push origin gh-pages --force
+# git checkout master
+# git rev-parse --abbrev-ref HEAD
