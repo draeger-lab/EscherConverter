5ff0e0ed4060feff201613005e589a4b7479d2d0
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-08-07 22:16:18 +0530

    Save log file.

--- frontend/EscherConverter/app.js
+++ frontend/EscherConverter/app.js
@@ -1,7 +1,12 @@
 // Complete JS code.
 console.log('start');
 
+var BASE_URL = "http://localhost:6969/api";
 var $ = require("cash-dom");
+var nanoajax = require("nanoajax");
+var saveAs = require('file-saver').saveAs;
+var file_row = $('.file')[0].cloneNode(true);
+var log_loaded = false;
 
 var App = {
   init: function () {
@@ -9,5 +14,24 @@ var App = {
   }
 };
 
+
+// Output log.
+$('#download-log-file').on('click', function () {
+  var id = $('#conversion-number-input').val();
+  if (log_loaded) {
+    // TODO: Save contents of output as file.
+  }
+  else {
+    nanoajax.ajax({ url: BASE_URL + '/convert/' + id + '/log'}, function (code, responseText) {
+      if (code/100 == 2) {
+        var file = new File([responseText], id + "_log.txt", {type: "text/plain;charset=utf-8"});
+        saveAs(file);
+      }
+      else {
+        alert('Log file not available!!!');
+      }
+    });
+  }
+});
 
 module.exports = App;
