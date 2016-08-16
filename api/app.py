303224365fa08c190f5ae183e7e73071908932f6
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-08-16 18:14:43 +0530

    Deploy script + serve UI from Flask.
    Now only server needs to be run.

--- api/app.py
+++ api/app.py
@@ -1,9 +1,31 @@
 from flask import Flask
+from flask import send_from_directory
+
 from api import api
 from config import config
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api')
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
+@app.route('/js/<path:path>')
+def scripts(path):
+    return send_from_directory('static/js', path)
+
+
+@app.route('/css/<path:path>')
+def styles(path):
+    return send_from_directory('static/css', path)
+
+
+@app.route('/<filename>.png')
+def images(filename):
+    return send_from_directory('static', filename + '.png')
 
 
 if __name__ == "__main__":
