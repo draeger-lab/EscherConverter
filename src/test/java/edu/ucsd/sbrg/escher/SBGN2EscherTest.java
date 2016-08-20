33be2596e9f9036191c790ec1ef48f5f17448af8
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-08-20 23:31:57 +0530

    Tests passing.

--- src/test/java/edu/ucsd/sbrg/escher/SBGN2EscherTest.java
+++ src/test/java/edu/ucsd/sbrg/escher/SBGN2EscherTest.java
@@ -1,9 +1,7 @@
 package edu.ucsd.sbrg.escher;
 
-import edu.ucsd.sbrg.escher.converters.SBGN2Escher;
 import edu.ucsd.sbrg.escher.helper.SbgnEscherTestTuple;
-import edu.ucsd.sbrg.escher.model.Canvas;
-import edu.ucsd.sbrg.escher.model.EscherMap;
+import edu.ucsd.sbrg.escher.model.Node;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.BeforeClass;
@@ -11,8 +9,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.sbgn.SbgnUtil;
-import org.sbgn.bindings.Bbox;
-import org.sbgn.bindings.Sbgn;
 
 import javax.xml.bind.JAXBException;
 import java.io.File;
@@ -23,7 +19,6 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.notNull;
 import static org.mockito.Mockito.times;
 import static org.powermock.api.mockito.PowerMockito.spy;
 
@@ -33,18 +28,12 @@ import static org.powermock.api.mockito.PowerMockito.spy;
 @RunWith(JUnitParamsRunner.class)
 public class SBGN2EscherTest {
 
-  private static File        file;
-  private static Sbgn        sbgn;
-  private static SBGN2Escher converter;
-  private static EscherMap   map;
-  private static int numberOfMetabolites;
   private static List<String> listOfFilePaths = Arrays.asList(
       "data/mapk_cascade.sbgn.xml",
-//      "data/central_plant_metabolism.sbgn.xml",
       "data/e_coli_core_metabolism.sbgn.xml"
   );
 
-  private static List<SbgnEscherTestTuple> list = new ArrayList<>();;
+  private static List<SbgnEscherTestTuple> list = new ArrayList<>();
 
   @BeforeClass
   public static void createTheWorld() throws JAXBException {
@@ -79,29 +68,20 @@ public class SBGN2EscherTest {
   @Parameters({
       "0", "1"
   })
-  public void canGetCanvasInfo(int index) {
-    SbgnEscherTestTuple tuple = list.get(index);
-    Canvas canvas = new Canvas();
-
-    canvas.setHeight(1000.0);
-    canvas.setWidth(1000.0);
-    canvas.setY(0.0);
-    canvas.setX(0.0);
-
-    assertEquals("failure - canvas info not matching", canvas, tuple.map.getCanvas());
-  }
-
-
-  @Test
-  @Parameters({
-      "0", "1"
-  })
   public void nodesShouldBeEqualTest(int index) {
     SbgnEscherTestTuple tuple = list.get(index);
+    long nodeCountWOMultiMarkers;
+    // This is done because multi-markers are created from arcs instead of glyphs.
+    nodeCountWOMultiMarkers = tuple.map
+        .getNodes()
+        .entrySet()
+        .stream()
+        .filter(e -> e.getValue().getType() != Node.Type.multimarker).count();
+
     assertEquals("failure - " + tuple.file.getName() + " doesn't have " + tuple
         .numberOfMetabolites +
         " nodes"
-        + ".", tuple.numberOfMetabolites, tuple.map.getNodesCount());
+        + ".", tuple.numberOfMetabolites, nodeCountWOMultiMarkers);
   }
 
 
@@ -124,7 +104,7 @@ public class SBGN2EscherTest {
   public void addCanvasCalled(int index) {
     SbgnEscherTestTuple tuple = list.get(index);
 
-    Mockito.verify(tuple.converter).addCanvasInfo((Bbox)notNull());
+    Mockito.verify(tuple.converter).addCanvasInfo(any());
   }
 
 }
